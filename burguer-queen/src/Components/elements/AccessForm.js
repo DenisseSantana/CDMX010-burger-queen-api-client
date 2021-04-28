a34d6478a3ec9a89a0ec8fac51c565f0c73e5f43
Denisse <ledennii@gmail.com>
2021-04-28 00:27:03 -0500

    CSS LogIn web and mobile

--- burguer-queen/src/Components/elements/AccessForm.js
+++ burguer-queen/src/Components/elements/AccessForm.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const AccessForm =()=>{
     return(
-        <form>
+        <form className='myForm'>
             <input type="text" id="username" name="username" placeholder='Usuario'/><br/><br/>
             <input type="password" id="password" name="password" placeholder='Contraseña'/><br/><br/>
             <input type="submit" value="Ingresar"/> 
