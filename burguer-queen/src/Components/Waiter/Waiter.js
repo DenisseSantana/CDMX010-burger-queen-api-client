8195e413f6bebe547a18ec414ef35af63a64eb45
Denisse <ledennii@gmail.com>
2021-05-07 09:59:14 -0500

    Prompt datos e impresion

--- burguer-queen/src/Components/Waiter/Waiter.js
+++ burguer-queen/src/Components/Waiter/Waiter.js
@@ -1,5 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import {Header} from '../elements/Header'
+// import { AlertOrder } from './AlertOrder';
 import {MenuElement} from './MenuElement'
 
 
@@ -27,18 +28,42 @@ export const Waiter =()=>{
     useEffect(()=>{
         handleGetData()
     },[]) 
-
+    
+    // const newOrder = ()=>{
+    // let goAlert = document.getElementbyID('toShowAlert')
+    // goAlert.innerHTML = <AlertOrder />
+    // }
+     const newOrder = ()=>{
+    let clientName = prompt('Nombre del cliente');
+    if(clientName === null || clientName ==''){
+        alert('No ingresaste el nombre del cliente')
+    }else{
+        let numberMesa = prompt('Ingresa No. de mesa');
+        if (numberMesa === null || numberMesa == ''){
+          alert('No Ingresaste no. de mesa')
+        } else {
+            alert('Los datos se guardaron correctamente!')
+        }
+    document.getElementById('myClientName').innerHTML= clientName;
+    document.getElementById('myTable').innerHTML= numberMesa;
+    }
+    // document.getElementById('myClientName').innerHTML= clientName.value;
+    // document.getElementById('myTable').innerHTML= numberMesa.value;
+    }
     
     return(
         <div className='waiterView'>
             <Header/>
             <div className='bottonSeccion'>
-                <div className='botonComponent'>
+                <div className='botonComponent' onClick={newOrder}> 
                     <p>Nueva orden</p>
                 </div>
                 <div className='botonComponent'>
                     <p>Ordeness</p>
                 </div>
+                {/* <div id='toShowAlert'>
+                    
+                </div> */}
             </div>
       
                 <div className='typeMenu' >
@@ -60,8 +85,8 @@ export const Waiter =()=>{
     
            <div className="toOrder">
                 <div className='nameColumns'>
-                    <h3>Cliente</h3>
-                    <h3>Mesa / No.</h3>
+                    <h3 id='myClientName'>Cliente</h3>
+                    <h3 id='myTable'>Mesa / No.</h3>
                     <h3>Orden</h3>
                 </div>
                 <div className='itemOrder'>
